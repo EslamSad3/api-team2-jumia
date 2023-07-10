39d1309b2404df550b2c103800395a76a02cf4f6
Eslam Saad <106920768+EslamSad3@users.noreply.github.com>
2023-07-11 02:31:55 +0300

    Latest USer

--- services/authService.js
+++ services/authService.js
@@ -38,7 +38,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   // 3) generate token
   // const token = createToken(user._id);
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+  const token = jwt.sign({ userId: user._id ,name:user.name}, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRE_TIME,
   });
 
